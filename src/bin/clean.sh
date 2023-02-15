4746bea84904476f11b410daa28b62b60a865165
Maarten <maarten.wullink@sidn.nl>
2023-02-15 14:26:39 +0100

    fix

--- src/bin/clean.sh
+++ src/bin/clean.sh
@@ -9,16 +9,6 @@ if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
-function show_usage()
-{
-  echo "Usage: $CURRENT_SCRIPT [service]+\n"
-  echo "Supported services: $SCRIPT_SUPPORT"
-}
-
-if [ "$#" -lt 1 ]; then
-    show_usage exit 1
-fi
-
 echo "All comoponents and data will be deleted on hosts"
 read -p "Are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
@@ -30,8 +20,4 @@ fi
 
 echo "Clean hosts"
 
-SCRIPT_SUPPORT="all"
-PB_PREFIX="clean"
-DEPLOY_MODE=$DEPLOY_MODE_COMP
-
-run_playbooks "$@"
+run_playbook "clean-all.yml"
