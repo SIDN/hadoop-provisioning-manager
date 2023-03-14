8594c9dbfc8644b59d41462cffa202dfa5decd32
Maarten <maarten.wullink@sidn.nl>
2023-03-14 15:42:30 +0100

    fix

--- src/bin/destroy.sh
+++ src/bin/destroy.sh
@@ -9,6 +9,12 @@ if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
+if [ "$#" -lt 1 ]; then
+    show_usage exit 1
+fi
+
+COMP=$1
+
 echo "Components and/or data will be deleted on hosts"
 read -p "Are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
@@ -18,10 +24,6 @@ then
     exit 1
 fi
 
-if [ "$#" -lt 1 ]; then
-    show_usage exit 1
-fi
-
-run_playbooks "destroy-$@.yml"
+run_playbooks "destroy-$COMP.yml"
 
 #run_playbook "destroy-all.yml"
